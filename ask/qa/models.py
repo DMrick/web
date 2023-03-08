a8d321d94d3388c1cb12d842f37af467d0946b15
DMrick <d.makarov84@gmail.com>
2023-03-08 19:41:42 +0300

    lesson23

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.order_by('-rating')
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField(blank=True)
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.DecimalField
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING,)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+    objects = QuestionManager()
+
+class Answer(models.Model):
+    text = models.TextField(blank=True)
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question, on_delete=models.DO_NOTHING)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING,)
--- hello.py
+++ hello.py
@@ -3,5 +3,5 @@ def app(environ, start_response):
     args = environ.get('QUERY_STRING').split("&")
     response_headers = [("Content-type", "text/plain")]
     start_response('200 OK', response_headers)
-    response_body = '\n'.join(args)
+    response_body = '\r\n'.join(args)
     return [response_body.encode('utf-8')]
